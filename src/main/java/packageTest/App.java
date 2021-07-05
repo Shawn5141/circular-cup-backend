25fff6ac1abe6c89805a3d3995144058c21d0501
Shou En Hsiao <shouen_hsiao@apple.com>
2021-07-05 12:27:48 -0700

    setup docker-compose

--- src/main/java/packageTest/App.java
+++ src/main/java/packageTest/App.java
@@ -1,5 +1,10 @@
 package packageTest;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
+@SpringBootApplication
+@EnableCaching
 public class App
 {
     public static void main( String[] args )
@@ -8,6 +13,7 @@ public class App
         int ans = ourSuperComputer.answer_to_the_ultimate_question_of_life_the_universe_and_everything();
 
         RedisJava cache = new RedisJava();
+        System.out.println("===get Host " + cache.get_host());
         cache.cache_value("answer_to_the_ultimate_question_of_life_the_universe_and_everything",String.valueOf(ans));
 
         System.out.println("Cached value: " + cache.get_cached_value("answer_to_the_ultimate_question_of_life_the_universe_and_everything"));
--- src/main/java/packageTest/RedisJava.java
+++ src/main/java/packageTest/RedisJava.java
@@ -1,12 +1,33 @@
 package packageTest;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
 import redis.clients.jedis.Jedis;
 
+@Configuration
+@AutoConfigureAfter(RedisAutoConfiguration.class)
+@EnableCaching
 public class RedisJava {
+
+    @Value("${spring.redis.host:localhost}")
+    private String redisHost;
+
+    @Value("${spring.redis.port:6379}")
+    private int redisPort;
 
     Jedis jedis;
     DeepThought super_computer;
     public RedisJava(){
-        this.jedis = new Jedis("localhost", 6379);
+        if(redisHost==null){
+            redisHost = "redis";
+        }
+        if(redisPort==0){
+            redisPort = 6379;
+        }
+        System.out.println("===redisHost"+ redisHost.toString()+" port :"+ redisPort);
+        this.jedis = new Jedis(redisHost, redisPort);
     }
 
     public void cache_value(String key, String value){
@@ -15,6 +36,10 @@ public class RedisJava {
 
     public String get_cached_value(String key){
         return this.jedis.get(key);
+    }
+
+    public String get_host() {
+        return this.redisHost;
     }
 
 }
