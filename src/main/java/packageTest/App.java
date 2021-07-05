08d07f25a9e52a8b576e5836c9f532955f694322
Shou En Hsiao <shouen_hsiao@apple.com>
2021-07-05 13:47:35 -0700

    use container maven:3.8.1-openjdk-11-slim

--- src/main/java/packageTest/App.java
+++ src/main/java/packageTest/App.java
@@ -13,7 +13,7 @@ public class App
         int ans = ourSuperComputer.answer_to_the_ultimate_question_of_life_the_universe_and_everything();
 
         RedisJava cache = new RedisJava();
-        System.out.println("===get Host " + cache.get_host());
+        System.out.println("===get Host"+cache.get_host());
         cache.cache_value("answer_to_the_ultimate_question_of_life_the_universe_and_everything",String.valueOf(ans));
 
         System.out.println("Cached value: " + cache.get_cached_value("answer_to_the_ultimate_question_of_life_the_universe_and_everything"));
